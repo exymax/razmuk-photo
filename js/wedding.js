c6a05194858aae67bd43d895679dbda4a651a822
Levenko, Vladimir <v.levenko@itransition.com>
2017-07-21 22:57:21 +0300

    Fixed gallery behavior

--- js/wedding.js
+++ js/wedding.js
@@ -4,6 +4,7 @@ $(document).ready(function() {
         var jsonID = $(this).data('json-id');
         $.getJSON('js/json/wedding/'+jsonID+'.json', function(data) {
             if(data.hasOwnProperty('photos')) {
+                console.log(data.photos);
                 data.photos.forEach(function(url) {
                     var gridItemWrapper = $('<div class="col-xs-4 grid-item"></div>');
                     var gridItem = gridItemWrapper.append('<img src="'+url+'" />');
@@ -12,7 +13,10 @@ $(document).ready(function() {
                 $('.grid-container').masonry({
                     itemSelector: '.grid-item',
                     columnWidth: '.grid-sizer'
-                });
+                }).fadeIn();
+                setTimeout(function() {
+                    $('.grid-container').masonry('layout');
+                }, 0);
             }
         });
         $('.photosets').fadeOut(300);
